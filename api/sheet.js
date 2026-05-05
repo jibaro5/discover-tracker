baf8c89eead2553c1cbe8c29696cea7da3735913
jibaro5 <omarj.cruzlopez@gmail.com>
2026-05-05 13:12:18 -0400

    Update sheet.js

--- api/sheet.js
+++ api/sheet.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
 
   if (req.method === "OPTIONS") { res.status(200).end(); return; }
 
-  const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyulP43RWyq8kkpDudVtGPyZLZZgNStaswZMIlKd-49SUoMWOAJjITbwMPwfQtaFgXy/exec";
+  const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwYQE4ALmgPj42yDxBcFlHiT0XFW6UijiRBz3DeBUmWH9k76wGl6QwH1_L-Y7XBj8Rc/exec";
 
   try {
     const response = await fetch(`${SCRIPT_URL}?action=read`, {
